4e76788cf9ea1c0bc76754f58604c307a6ad47d5
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2022-11-01 22:50:52 +0530

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Priyanka Sachan</title>
+        <meta
+          name="description"
+          content="Hi, I am Priyanka Sachan. I build things for web (From mobile to desktop and dicentralized systems)."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Hello!</h1>
+      </div>
+    </div>
+  );
+}
