770bcdab4ad27f58db40c8a8b2bef43960db64e1
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2022-11-01 23:00:30 +0530

    Add tailwind

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div className="container">
         <h1>Hello!</h1>
       </div>
     </div>
