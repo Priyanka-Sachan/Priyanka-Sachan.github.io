7da6810ea11b5f3f78eb383b1c23b751ab755c8b
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2022-12-26 15:05:39 +0530

    Update links

--- components/SocialBar.js
+++ components/SocialBar.js
@@ -4,23 +4,28 @@ import Image from 'next/image'
 export default function SocialBar() {
   return (
     <div className="md:fixed md:bottom-0 md:left-5">
-      <div className="grid grid-cols-4 md:block justify-items-center max-w-fit md:max-w-none mx-auto">
-        <div className="py-2 px-2">
+      <div className="grid grid-cols-5 md:block justify-items-center max-w-fit md:max-w-none mx-auto">
+        <div className="py-2 px-2 hover:pt-1 hover:pb-3">
           <a href={socialLinks.github}>
             <Image src="/svg/github.svg" width="24" height="24" />
           </a>
         </div>
-        <div className="py-2 px-2">
+        <div className="py-2 px-2 hover:pt-1 hover:pb-3">
+          <a href={socialLinks.instagram}>
+            <Image src="/svg/instagram.svg" width="24" height="24" />
+          </a>
+        </div>
+        <div className="py-2 px-2 hover:pt-1 hover:pb-3">
           <a href={socialLinks.twitter}>
             <Image src="/svg/twitter.svg" width="24" height="24" />
           </a>
         </div>
-        <div className="py-2 px-2">
+        <div className="py-2 px-2 hover:pt-1 hover:pb-3">
           <a href={socialLinks.linkedin}>
             <Image src="/svg/linkedin.svg" width="24" height="24" />
           </a>
         </div>
-        <div className="py-2 px-2">
+        <div className="py-2 px-2 hover:pt-1 hover:pb-3">
           <a href={socialLinks.codepen}>
             <Image src="/svg/codepen.svg" width="24" height="24" />
           </a>
