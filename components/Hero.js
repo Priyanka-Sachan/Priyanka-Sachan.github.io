3b52ba740c3faae1bb4dec62e5e60153baf16f65
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2022-12-30 01:52:46 +0530

    Add blog

--- components/Hero.js
+++ components/Hero.js
@@ -1,3 +1,5 @@
+import { socialLinks } from '../public/data.json'
+
 export default function Hero() {
   return (
     <section className="mt-28 mb-40">
@@ -6,6 +8,19 @@ export default function Hero() {
       <h1 className="text-6xl text-slate-500 py-4">I build things for web. </h1>
       <h3 className="text-md text-slate-900 pt-4 pb-16">
         From mobile to desktop and decentralized systems
+      <br></br>
+        And then log it in my
+        <p className="relative group inline pl-2">
+          <a
+            href={socialLinks.blog}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-700 dark:text-white"
+          >
+            second 🧠 
+          </a>
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-500 transition-all group-hover:w-20"></span>
+        </p>
       </h3>
     </section>
   )
